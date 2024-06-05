04a17f86ea7c8ac13891d6f946e9aae102df8e74
MerakiShubh <shubhamt.champ@gmail.com>
2024-06-06 01:13:20 +0530

    Add create book endpoint

--- src/HTTP/api.ts
+++ src/HTTP/api.ts
@@ -1,9 +1,19 @@
+import useTokenStore from "@/store";
 import axios from "axios";
+
 const api = axios.create({
   baseURL: "http://localhost:5183",
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use((config) => {
+  const token = useTokenStore.getState().token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export const login = async (data: { email: string; password: string }) =>
@@ -16,3 +26,10 @@ export const register = async (data: {
 }) => api.post("/api/users/register", data);
 
 export const getBooks = async () => api.get("/api/books");
+
+export const createBook = async (data: FormData) =>
+  api.post("/api/books", data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
